ff24d6a3225c6f1018bd1c6cac4bd1a5b81d5dfb
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-11-19 23:13:31 -0800

    created list to hold incorrect answers

--- quiz.py
+++ quiz.py
@@ -1,24 +1,28 @@
 total_score = 0
-
+incorrect_questions = []
 def question_one():
-    response = input("Make School is located in San Francisco, true or false? Enter a 0 for false and a 1 for true: ")
+    question = "Make School is located in San Francisco, true or false? Enter a 0 for false and a 1 for true: "
+    response = input(question)
     correct_answer = "1"
     if response == correct_answer:
         print("Correct!")
         return 1
     elif response != correct_answer:
         print("Sorry, wrong answer!")
+        incorrect_questions.append(question)
         return 0
 
 
 def question_two():
-    response = input("How many concentrations are there at Make School? Enter answer in form of integer: ")
+    question = "How many concentrations are there at Make School? Enter answer in form of integer: "
+    response = input(question)
     correct_answer = "4"
     if response == correct_answer:
         print("Correct!")
         return 1
     elif response != correct_answer:
         print("Sorry, wrong answer!")
+        incorrect_questions.append(question)
         return 0
 
 #Code to run the quiz
@@ -28,3 +32,4 @@ question_two()
 total_score += points
 
 print(total_score)
+print(incorrect_questions)
